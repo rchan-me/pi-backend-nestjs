43a3a9ea453c37bbf777b266bb06d4d825686753
Rachata Chantarasombat <rachata.chantarasombat@gmail.com>
2023-08-11 03:28:34 +0700

    feat: set up prisma and init schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import prismaConfig from './config/prisma.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        prismaConfig,
+      ],
+      envFilePath: ['.env'],
+      expandVariables: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
