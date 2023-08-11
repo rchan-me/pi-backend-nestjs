e3555136d92be18e67e046fd95be945566b082f5
RC <roc75258@hotmail.com>
2023-08-11 16:24:23 +0700

    convert to DTO to allow API annotation

--- src/user/user.type.ts
+++ src/user/user.type.ts
@@ -1,4 +1,9 @@
-export type UserData = {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserData {
+  @ApiProperty({ required: true })
   name: string;
+
+  @ApiProperty({ required: true })
   email: string;
-};
+}
