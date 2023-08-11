929b65b9bf51001aada986ff20e1ee9593a9f8fb
RC <roc75258@hotmail.com>
2023-08-11 16:23:34 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get<number>('app.port');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Pi Securities Backend')
+    .setDescription('List of User APIs for quick API contract references')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(port);
 }
 bootstrap();
